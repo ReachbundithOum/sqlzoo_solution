38837f23583dc188636446cde92dce64e8a8e17b
Oum Reachbundith <Reachbundith@ardb.com.kh>
2023-07-28 09:09:11 +0700

    modify select_select add 2 files quiz_select & sum_count

--- select_select.sql
+++ select_select.sql
@@ -48,3 +48,15 @@
     SELECT continent, name
     FROM world x
     WHERE name <= ALL (SELECT name FROM world y WHERE y.continent = x.continent);
+-- Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+--#9/
+    SELECT name,continent,population
+    FROM world x
+    WHERE 25000000 > ALL
+    (SELECT population FROM world y WHERE x.continent = y.continent AND y.population > 0);
+-- Some countries have populations more than three times that of all of their neighbours (in the same continent). Give the countries and continents.
+--#10/
+    SELECT name,continent
+    FROM world x
+    WHERE population > ALL(SELECT population * 3 FROM world y
+    WHERE x.continent = y.continent AND population > 0 AND y.name != x.name);
